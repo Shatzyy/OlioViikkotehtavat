6cbd6ec31ed8a73250e7988fa2188e7b5dd334a2
Shatzyy <44499570+Shatzyy@users.noreply.github.com>
2020-02-20 22:50:20 +0200

    Viikko7_1

---
+++ Viikko7/Viikko7_1/app/src/main/java/com/example/viikko7_1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.viikko7_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printHelloWorld(View v) {
+        System.out.println("Hello World!");
+    }
+}
