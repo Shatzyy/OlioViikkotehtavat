9713e6d6b56f14b1292c1e3f3dd2971718a476d8
Shatzyy <44499570+Shatzyy@users.noreply.github.com>
2020-04-27 17:23:12 +0300

    Setting up toolbar & navigation drawer
    - Created project
    - Added toolbar
    - Added navigation drawer

---
+++ Harjoitustyo/JavaBank/app/src/main/java/com/example/javabank/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.javabank;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.drm.DrmStore;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set drawer for navigation
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    // Method for closing drawer when back is pressed, if it is open
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
